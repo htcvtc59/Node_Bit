9d092c7ab201e1043938dfb837df97eb41c124ce
htcvtc59 <htcvtc59@gmail.com>
2017-12-30 02:17:42 +0700

    nodejs bitweb

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('https://app-bit-nodejs.herokuapp.com/');
+// http://localhost:3000
 
 var accountlogin = {
     acc_name: null,
